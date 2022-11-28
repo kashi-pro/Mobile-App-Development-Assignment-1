d2827a0c843a27b6e6852f60796096c19dd1d575
kashi-pro <kashanbabar99@gmail.com>
2022-11-28 21:14:58 +0500

    first commit

---
+++ app/src/main/java/com/assignment/assignment_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.assignment.assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.q1_l1_relative);
+
+    }
+
+}
